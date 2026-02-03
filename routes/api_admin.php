c318c5897e010a498f61cea4298690e10a6e5ddf
thulirsoft <thulirsoftware@gmail.com>
2026-02-03 16:24:07 +0530

    Added APIs to create questions with choices

--- routes/api_admin.php
+++ routes/api_admin.php
@@ -48,8 +48,10 @@ Route::prefix('admin')->middleware('admin')->group(function () {
 
     // Questions
     Route::get('assessments/{assessment_id}/questions', [AssessmentQuestionController::class, 'index']);
-    Route::post('assessments/{assessment_id}/questions', [AssessmentQuestionController::class, 'store']);
-    Route::put('questions/{id}', [AssessmentQuestionController::class, 'update']);
+    Route::post('assessments/{assessment_id}/questions', [AssessmentQuestionController::class, 'store']); 
+    Route::post('assessments/{assessment_id}/questions/with-choices', [AssessmentQuestionController::class, 'storeWithChoices']);
+    Route::put('questions/{id}', [AssessmentQuestionController::class, 'update']); 
+    Route::put('questions/{id}/with-choices', [AssessmentQuestionController::class, 'updateWithChoices']);
     Route::delete('questions/{id}', [AssessmentQuestionController::class, 'destroy']);
 
     // Choices
