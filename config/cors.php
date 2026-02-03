02758c399c5d106ef62f09599c0a20022d26525f
thulirsoft <thulirsoftware@gmail.com>
2026-02-03 15:31:03 +0530

    Added cors plus cache

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,42 @@
 <?php
 
 return [
-    'paths' => ['*'],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => [
+        'api/v1/*',
+        'web/v1/*',
+    ],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
+
+    'allowed_origins' => [
+        'http://localhost:3000', // CRA
+        'http://localhost:5173', // Vite
+        'http://localhost:8080', // Next / custom
+        'http://127.0.0.1:3000',
+        'https://seashell-okapi-169452.hostingersite.com',
+    ],
+
+    'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => true,
 ];
