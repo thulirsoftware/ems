f7f028127db6d367cb13f1ca7ed6dac1eb8a11a6
thulirsoft <thulirsoftware@gmail.com>
2026-02-05 16:48:45 +0530

    Modified cors

--- config/cors.php
+++ config/cors.php
@@ -25,6 +25,9 @@ return [
     'allowed_origins' => [
         'http://localhost:3000', // CRA
         'http://localhost:5173', // Vite
+        'http://localhost:5174', // Vite
+        'http://localhost:5175', // Vite
+        'http://localhost:5176', // Vite
         'http://localhost:8080', // Next / custom
         'http://127.0.0.1:3000',
         'https://seashell-okapi-169452.hostingersite.com',
